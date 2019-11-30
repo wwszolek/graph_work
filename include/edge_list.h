10a16fc24bb67378d5d806495dd65ffb9d03b595
wwszolek <wojtusiek1@gmail.com>
2019-11-30 02:53:07 +0100

    top only edge_list

--- include/edge_list.h
+++ include/edge_list.h
@@ -8,10 +8,11 @@
 class edge_list {
 private:
 	list<3> _list;
+	bool _top_only;
 	int _size;
 public:
 	edge_list();
-	edge_list(const std::string&);
+	edge_list(const std::string&,bool);
 
 
 	void show();
--- source/edge_list.cpp
+++ source/edge_list.cpp
@@ -4,17 +4,29 @@
 #include <fstream>
 
 edge_list::edge_list() :_list(), _size(-1) {}
-edge_list::edge_list(const std::string& filename) {
+edge_list::edge_list(const std::string& filename,bool t):_top_only(t) {
 	std::fstream file(filename);
 
 	file >> _size;
 
-	for (int i = 0; i < _size; i++) {
-		for (int j = 0; j < _size; j++) {
-			int v;
-			file >> v;
-			if (v)
-				_list.insertHead({ i, j, v });
+	if (t) {
+		for (int i = 0; i < _size; i++) {
+			for (int j = i; j < _size; j++) {
+				int v;
+				file >> v;
+				if (v)
+					_list.insertHead({ i, j, v });
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < _size; i++) {
+			for (int j = 0; j < _size; j++) {
+				int v;
+				file >> v;
+				if (v)
+					_list.insertHead({ i, j, v });
+			}
 		}
 	}
 }
